90d252892ed7d54fd268d292e6b96ee27e3dbd8b
ClaudioDev <aleledesma687@gmail.com>
2026-01-30 00:09:47 -0300

    Add basic views, routing and page templates

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def projects(request):
+    projects = Project.objects.all()
+    return render(request, 'projects.html', {'projects': projects})
+
+def contact(request):
+    return render(request, 'contact.html')
