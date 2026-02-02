7dea4676c73a6d255af1b88ea872fe402a445c59
ClaudioDev <aleledesma687@gmail.com>
2026-02-02 00:30:44 -0300

    Add working contact form with email sending

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,3 +123,14 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Project
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def home(request):
@@ -13,5 +15,32 @@ def projects(request):
     return render(request, 'portfolio/projects.html', {'projects': projects})
 
 def contact(request):
-    return render(request, 'portfolio/contact.html')
+    succes = False
+    
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        
+        full_message = f"""
+    
+        New contact message from portfolio:
+        
+        Name: {name}
+        Email: {email}
+        
+        Message:
+        {message}
+        """
 
+        send_mail(
+            subject="New Portfolio Contact Message",
+            message=full_message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[settings.EMAIL_HOST_USER],
+            fail_silently=False
+        )
+        
+        succes = True
+    
+    return render(request, 'portfolio/contact.html', {'success': succes})
